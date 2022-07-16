bfd8b25a052267496648372bb9523af203f6374a
KTLDTU <72263629+KTLDTU@users.noreply.github.com>
2022-07-17 01:52:42 +0200

    added controller for velocity

---
+++ Assets/Scripts/VelocityController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class VelocityController : MonoBehaviour
+{
+    private static readonly float MIN_VELOCITY = 0.05f;
+    private static readonly float MIN_ANGULARVELOCITY = 0.03f;
+
+    [SerializeField]
+    private Rigidbody _rigidBody;
+
+    private int _collisionCounter = 0;
+    private bool IsVelocityLow => _rigidBody.velocity.magnitude <= MIN_VELOCITY;
+    private bool IsAngularVelocityLow => _rigidBody.angularVelocity.magnitude <= MIN_ANGULARVELOCITY;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DisableVelocity();
+    }
+
+    private void DisableVelocity()
+    {
+        if (IsVelocityLow && IsAngularVelocityLow && _collisionCounter > 0)
+        {
+            _rigidBody.isKinematic = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _collisionCounter++;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _collisionCounter--;
+    }
+}
